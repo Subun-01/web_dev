7486a585447b339957e2fceaa05b187916db94e6
Soumitra Naik <128518565+Subun-01@users.noreply.github.com>
2024-03-04 20:15:56 +0000

    datatyoe

---
+++ basics/dataType.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3); // for browser
+
+let name = "sn"
+let age= 12
+let is= true
+let state;
+
+// number => 2 to power 53
+//bigint
+//string --> ""
+// boolean
+//null -->stand alone value
+//undefined -->
+// symbol --> unique
+
+//odject
+
+console.log(typeof age);
